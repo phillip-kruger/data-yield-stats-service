ef92a2bff5603cd85a783bf5d692fd466f52ec0e
Kannan <kannan@property.xyz>
2021-04-01 13:45:45 +0530

    fix: houseType validation

--- src/main/java/xyz/property/data/validator/HouseTypeValidator.java
+++ src/main/java/xyz/property/data/validator/HouseTypeValidator.java
@@ -5,20 +5,31 @@ import xyz.property.data.model.HouseType;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class HouseTypeValidator implements ConstraintValidator<ValidHouseType, String> {
 
+    List<String> valueList = null;
+
     @Override
     public void initialize(ValidHouseType constraintAnnotation) {
+        valueList = new ArrayList<>();
+
+        @SuppressWarnings("rawtypes")
+        Enum[] enumValArr = HouseType.class.getEnumConstants();
+
+        for (@SuppressWarnings("rawtypes") Enum enumVal : enumValArr) {
+            valueList.add(enumVal.toString().toUpperCase());
+        }
     }
 
     @Override
     public boolean isValid(String houseType, ConstraintValidatorContext constraintValidatorContext) {
         AtomicBoolean response = new AtomicBoolean(false);
         try {
-            Enum.valueOf(HouseType.class, houseType);
-            response.set(true);
+            response.set(valueList.contains(houseType.toUpperCase()));
         } catch (IllegalArgumentException e) {
             response.set(false);
         }

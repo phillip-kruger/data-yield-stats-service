f8f617905f3d5faebfe6ccc9c8fa9db731d6a994
sdicola <simone@property.xyz>
2021-09-09 17:01:10 +0200

    chore: add graphql handler

--- src/main/java/xyz/property/data/handlers/GraphqlHandler.java
+++ src/main/java/xyz/property/data/handlers/GraphqlHandler.java
@@ -1,11 +1,11 @@
 package xyz.property.data.handlers;
 
-
 import io.smallrye.mutiny.Uni;
-import org.eclipse.microprofile.graphql.*;
+import org.eclipse.microprofile.graphql.Description;
+import org.eclipse.microprofile.graphql.GraphQLApi;
+import org.eclipse.microprofile.graphql.Query;
 import xyz.property.cache.CacheKey;
 import xyz.property.cache.Cached;
-import xyz.property.data.converters.PostCodeConverter;
 import xyz.property.data.model.YieldStats;
 import xyz.property.data.resources.YieldResource;
 import xyz.property.data.resources.YieldSearchCriteria;
@@ -19,9 +19,6 @@ public class GraphqlHandler {
 
     @Inject
     YieldResource yieldResource;
-
-    @Inject
-    PostCodeConverter postCodeConverter;
 
     @Query
     @Description("Fetch yield stats for the give full postcode")
